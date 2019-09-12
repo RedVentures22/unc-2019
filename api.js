b560509d67fb938c93fdeaa69de5c4b88b400385
gjacks13 <gjackson@redventures.com>
2019-09-12 18:10:50 -0400

    added get routes for pets

---
+++ api.js
@@ -0,0 +1,43 @@
+const express = require('express')
+const app = express();
+const port = 3001;
+
+const pets = [
+  {
+    name: 'spot',
+    type: 'dog',
+    breed: 'poodle',
+    color: 'black',
+    owner: 'chris tucker',
+    id: 1,
+  },
+  {
+    name: 'coco',
+    type: 'dog',
+    breed: 'terrier',
+    color: 'brown',
+    owner: 'mani',
+    id: 2,
+  }
+];
+
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.get('/pets', (req, res) => {
+  res.json(pets);
+});
+
+app.get('/pets/:petId', (req, res) => {
+  const pet = pets.find(pet => pet.id === +req.params.petId)
+
+  if (!pet) {
+    console.error(`Pet ID ${req.params.petId} was not found`);
+    return res.status(404).send();
+  }
+
+  res.json(pet);
+});
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
